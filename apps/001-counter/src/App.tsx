7ac46eccc1e96676d64e98fb7c16be9d380dbd92
m.y <masaki.yamadalv1@gmail.com>
2025-12-29 22:48:19 +0900

    処理 記述 次工程はコンポーネント化

--- apps/001-counter/src/App.tsx
+++ apps/001-counter/src/App.tsx
@@ -1,17 +1,42 @@
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleDecrement = () => {
+    setCount((prev) => prev - 1);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+  };
+
+  const handleIncrement = () => {
+    setCount((prev) => prev + 1);
+  };
+
   return (
     <div className="flex justify-center items-center w-screen h-screen bg-teal-100">
       <div className="size-100 p-10 bg-white rounded-md">
         <h1 className="text-3xl text-center mb-10">Counter App</h1>
-        <div className="text-9xl text-center font-bold mb-10">0</div>
+        <div className="text-9xl text-center font-bold mb-10">{count}</div>
         <div className="flex justify-around items-center">
-          <button className="px-4 py-2 text-2xl text-white font-bold bg-orange-500 rounded">
+          <button
+            className="px-4 py-2 text-2xl text-white font-bold bg-orange-500 rounded cursor-pointer hover:opacity-75"
+            onClick={handleDecrement}
+          >
             -
           </button>
-          <button className="px-4 py-2 text-2xl text-white font-bold bg-black rounded">
+          <button
+            className="px-4 py-2 text-2xl text-white font-bold bg-black rounded cursor-pointer hover:opacity-75"
+            onClick={handleReset}
+          >
             reset
           </button>
-          <button className="px-4 py-2 text-2xl text-white font-bold bg-blue-500 rounded">
+          <button
+            className="px-4 py-2 text-2xl text-white font-bold bg-blue-500 rounded cursor-pointer hover:opacity-75"
+            onClick={handleIncrement}
+          >
             +
           </button>
         </div>

4efc76642e566351a80c83b3824ce1240f884ee3
m.y <masaki.yamadalv1@gmail.com>
2025-12-29 23:34:51 +0900

    型宣言 実装

--- apps/001-counter/src/components/ActionButton.tsx
+++ apps/001-counter/src/components/ActionButton.tsx
@@ -1,4 +1,14 @@
-export const ActionButton = ({ label, bgColor, onClick }) => {
+type ActionButtonProps = {
+  label: string;
+  bgColor: string;
+  onClick: () => void;
+};
+
+export const ActionButton = ({
+  label,
+  bgColor,
+  onClick,
+}: ActionButtonProps) => {
   return (
     <button
       className={`px-4 py-2 text-2xl text-white font-bold ${bgColor} rounded cursor-pointer hover:opacity-75`}

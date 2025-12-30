e66b1d0446f7bf6c856765d21b4d4f1f6ec7533f
m.y <masaki.yamadalv1@gmail.com>
2025-12-30 17:59:16 +0900

    UI実装 仮作成

--- apps/002-click-game/src/App.tsx
+++ apps/002-click-game/src/App.tsx
@@ -1,8 +1,16 @@
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="flex justify-center items-center bg-white w-screen h-screen">
+      <div className="bg-white w-md h-96 rounded-lg border border-gray-300 shadow-lg">
+        <h1 className="p-4 text-xl font-bold">クリック連打ゲーム</h1>
+        <div className="p-4 pt-0">
+          <button className="bg-red-50 relative flex flex-col justify-center items-center p-4 w-full min-h-64 rounded-lg cursor-pointer">
+            <div className="text-5xl font-bold">Click me!</div>
+            <p className="text-gray-500">クリックしてスタート</p>
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 

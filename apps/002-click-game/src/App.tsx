db17c0a8e7bfad7d491e368875e6a92270a3378d
m.y <masaki.yamadalv1@gmail.com>
2026-01-04 02:53:44 +0900

    スタート画面UI作成 Start.tsx

--- apps/002-click-game/src/App.tsx
+++ apps/002-click-game/src/App.tsx
@@ -10,14 +10,16 @@ function App() {
   const [phase, setPhase] = useState<GamePhase>("start");
 
   return (
-    <div>
-      <div>
-        <h1>クリック連打ゲーム</h1>
-      </div>
-      <div>
-        {phase === "start" && <Start />}
-        {phase === "playing" && <Playing />}
-        {phase === "result" && <Resutl />}
+    <div className="flex justify-center items-center h-screen bg-blue-50">
+      <div className="w-full max-w-md h-80 p-4 bg-white rounded-md">
+        <div className="mb-4">
+          <h1 className="text-xl">クリック連打ゲーム</h1>
+        </div>
+        <div className="h-50 p-4 bg-amber-50 rounded-md">
+          {phase === "start" && <Start />}
+          {phase === "playing" && <Playing />}
+          {phase === "result" && <Resutl />}
+        </div>
       </div>
     </div>
   );
--- apps/002-click-game/src/components/Start.tsx
+++ apps/002-click-game/src/components/Start.tsx
@@ -1,3 +1,14 @@
 export const Start = () => {
-  return <div>Start</div>;
+  return (
+    <div>
+      <div className="w-full h-30 mb-4 bg-white rounded-md">
+        <button className="w-full h-full text-xl cursor-pointer">
+          クリックしてスタート
+        </button>
+      </div>
+      <p className="text-sm text-center">
+        10秒間で何回クリックできるか試してみよう
+      </p>
+    </div>
+  );
 };

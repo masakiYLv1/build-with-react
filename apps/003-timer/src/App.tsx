6750dbdee0b38effeda553b821d154836544686c
m.y <masaki.yamadalv1@gmail.com>
2026-01-10 16:51:40 +0900

    現在の状態がわかるUI実装

--- apps/003-timer/src/App.tsx
+++ apps/003-timer/src/App.tsx
@@ -35,7 +35,7 @@ function App() {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="w-full max-w-md h-80 p-4 bg-lime-100 rounded-md flex flex-col">
-        <Header />
+        <Header status={status} />
         <main className="flex-1 flex items-center justify-center">
           <TimeDisplay elapsedTime={elapsedTime} />
         </main>
--- apps/003-timer/src/Header.tsx
+++ apps/003-timer/src/Header.tsx
@@ -1,8 +1,21 @@
-export const Header = () => {
+import type { statusTime } from "./App";
+
+type HeaderProps = {
+  status: statusTime;
+};
+
+export const Header = ({ status }: HeaderProps) => {
+  const statusText =
+    status === "running"
+      ? "計測中"
+      : status === "stopped"
+      ? "停止中"
+      : "待機中";
+
   return (
     <div className="flex justify-between items-center px-2">
       <h1 className="text-lg">タイマー</h1>
-      <span className="text-sm text-gray-600">停止中</span>
+      <span className="text-sm text-gray-600">{statusText}</span>
     </div>
   );
 };

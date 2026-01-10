82094a35ded1f7fb47c4efc5bb0169179f21e73c
m.y <masaki.yamadalv1@gmail.com>
2026-01-10 19:10:14 +0900

    vite.config.ts fix

--- apps/003-timer/vite.config.ts
+++ apps/003-timer/vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/build-with-react/apps/003-timer/",
   plugins: [react(), tailwindcss()],
 });
